88249ea5d791fb5751e11b5cfdbbe716c5fd79c6
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2026-01-08 14:07:46 -0300

    feat: implement base JPA Specification infrastructure Add the foundational classes and generic helper for the JPA Specification pattern. This infrastructure enables the creation of dynamic, type-safe, and reusable database queries across all services.

---
+++ contact-management/src/main/java/spring/boot/contact_management/commom/specification/SearchCriteria.java
@@ -0,0 +1,14 @@
+package spring.boot.contact_management.commom.specification;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SearchCriteria <T extends Comparable<T>> {
+    private String key;
+    private String operation;
+    private T value;
+}
